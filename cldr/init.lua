e7c97232538dd13f8933c3e401d61dd7f46d5cd1
Caleb Maclennan <caleb@alerque.com>
2019-10-12 16:20:01 +0300

    Turn locale array into pl.Set()

--- cldr/init.lua
+++ cldr/init.lua
@@ -1,8 +1,16 @@
+-- External dependencies
+local Set = require("pl.Set")
+
+-- Internal modules
 local CLDR = {}
 
 setmetatable(CLDR, {
     __index = function (self, key)
-      self[key] = require("cldr.data." .. key)
+      local data = require("cldr.data." .. key)
+      if key == "locales" then
+        data = Set(data)
+      end
+      self[key] = data
       return self[key]
     end
   })
--- spec/cldr_spec.lua
+++ spec/cldr_spec.lua
@@ -11,4 +11,10 @@ describe('cldr', function ()
     assert.equal(542, #CLDR.locales)
   end)
 
+  it('should have truthy set accessors to locales', function ()
+    assert.truthy(CLDR.locales.en)
+    assert.truthy(CLDR.locales["en-GB"])
+    assert.falsy(CLDR.locales["und"])
+  end)
+
 end)
